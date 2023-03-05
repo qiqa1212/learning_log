46f84d51f10baf18ae4bf23cf23d9a871ceb6074
qiqa1212 <tumishgo@gmail.com>
2023-03-05 12:08:49 +0200

    done f part

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
