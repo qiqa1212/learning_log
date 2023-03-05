94ee9160fe778e2fcac4d5a7871ab24073306083
qiqa1212 <tumishgo@gmail.com>
2023-03-05 13:11:44 +0200

    f end and debug

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Topic(models.Model):
-    ...
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
 
 
 class Entry(models.Model):
